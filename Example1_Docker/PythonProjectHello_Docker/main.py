37a8e8bc19fa156862ac81a49a8fdb164d2e9e2d
ifanzilka <nabiyarov.fanzil@gmail.com>
2020-12-03 20:10:46 +0500

    add Example1

---
+++ Example1_Docker/PythonProjectHello_Docker/main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+while True:
+    print("Hello, world!\n")
+    time.sleep(1)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
