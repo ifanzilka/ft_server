ec67e5b15ff68e87144c8197b57fb0ba7b51f612
Blood Marillion <bmarilli@ev-o3.kzn.21-school.ru>
2020-12-08 19:28:44 +0300

    change

--- my_server/srcs/db.sh
+++ my_server/srcs/db.sh
@@ -11,3 +11,5 @@ mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRA
 mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
 mysql -e "FLUSH PRIVILEGES;"
 
+#phpmyadmin database
+mysql < /var/www/phpmyadmin/sql/create_tables.sql
