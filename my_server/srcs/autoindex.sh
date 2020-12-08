66a9c7d0d01ee7e7ddf4849c43abbc5f23f1f57c
Blood Marillion <bmarilli@ev-o3.kzn.21-school.ru>
2020-12-09 02:16:20 +0300

    add auto

--- my_server/srcs/autoindex.sh
+++ my_server/srcs/autoindex.sh
@@ -3,9 +3,11 @@ STR=$(cat /etc/nginx/sites-available/nginx.conf)
 SUB='autoindex on'
 if [[ $STR == *$SUB* ]]
 then
+	# s/регулярное_выражение/замена/флаги
 	sed -i 's/autoindex on/autoindex off/' /etc/nginx/sites-available/nginx.conf
 	echo "autoindex off"
 else
+	# s/регулярное_выражение/замена/флаги	
 	sed -i 's/autoindex off/autoindex on/' /etc/nginx/sites-available/nginx.conf
 	echo "autoindex on"
 fi
